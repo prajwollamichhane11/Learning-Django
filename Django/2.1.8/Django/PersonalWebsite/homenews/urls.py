737aceffd8c3f48c2d5ac755ffab2eb8af99d41b
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-05-26 08:59:53 +0545

    Dynamic

--- Django/2.1.8/Django/PersonalWebsite/homenews/urls.py
+++ Django/2.1.8/Django/PersonalWebsite/homenews/urls.py
@@ -8,5 +8,6 @@ from . import views
 #we specified the path
 #home is a fucntion on views.py of this app
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('add',views.add,name='add')
 ]
--- Django/2.1.8/Django/PersonalWebsite/homenews/views.py
+++ Django/2.1.8/Django/PersonalWebsite/homenews/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html',{'name':'Prajwol'})
+
+def add(request):
+    return render(request,'result.html')
