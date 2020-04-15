2c6d6bf31b793c6044a158bccd9b1e53c904a5c0
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-04-16 00:50:37 +0545

    New Updates

--- Django/PersonalWebsite/travello/urls.py
+++ Django/PersonalWebsite/travello/urls.py
@@ -8,5 +8,6 @@ from . import views
 #we specified the path
 #home is a fucntion on views.py of this app
 urlpatterns=[
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path('about/',views.AboutPageView.as_view(), name='about'),
 ]
--- Django/PersonalWebsite/travello/views.py
+++ Django/PersonalWebsite/travello/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 news1 = "यार्सा टिप्न लेकतिर लर्को विद्यालय बन्द"
 news2 = "के छ अर्थमन्त्रीले पूर्वअर्थमन्त्रीलाई पढाएको जीडीपीको अवस्था"
@@ -11,3 +13,6 @@ news4 = "हिरासतभित्रै युवक झुण्डिए
 # , {'news2':news2}, {'news3':news3}, {'news4':news4}
 def index(request):
     return render(request,'index.html', {'news1':news1})
+
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
